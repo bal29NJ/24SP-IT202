d67979abff712c776d5f0337c784a9f63b3563c9
bal29NJ <bal29@njit.edu>
2024-02-02 12:42:01 -0500

    New chapter
    new chapter files

---
+++ html_and_css_example/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <h5>
+        <p>Ben, Feb 2, 2024, IT-202-006, bal29@njit.edu</p>
+    </h5?>
+</footer>
---
+++ html_and_css_example/header.php
@@ -0,0 +1,7 @@
+<header>
+    <img src="html_image.png" alt="html_image"
+    width="58" />
+    <!-- inline style -->
+    <h1 style="color #272932; background-color: #828489;">Course Name: Advanced Internet Applications</h1>
+    <h2>Course Subject: IT-302</h1>
+</header>
